dada8e90e11088b214695d4ab56ce2115ec53e0b
Carlos Andres Galvis Padilla <48323392+Cgalvispadilla@users.noreply.github.com>
2020-05-13 11:20:05 -0500

    Initial commit

---
+++ app/src/main/java/com/example/appcecy/clases/Cliente.java
@@ -0,0 +1,4 @@
+package com.example.appcecy.clases;
+//la clase cliente hereda de la clase padre persona
+public class Cliente extends Persona{
+}
---
+++ app/src/main/java/com/example/appcecy/clases/Modista.java
@@ -0,0 +1,6 @@
+package com.example.appcecy.clases;
+
+//la clase modista hereda de la clase padre persona
+public class Modista extends Persona {
+
+}
