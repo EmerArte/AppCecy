377962b4f313bca9f67065f3d8f5bc70ace70af9
EmerArte <48594349+EmerArte@users.noreply.github.com>
2020-05-23 13:08:28 -0500

    layout olvidar contraseña hecho

---
+++ app/src/main/java/com/example/appcecy/clases/olvidarPass.java
@@ -0,0 +1,16 @@
+package com.example.appcecy.clases;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appcecy.R;
+
+public class olvidarPass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_olvidar_pass);
+    }
+}
